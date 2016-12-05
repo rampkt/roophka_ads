2d56746f3bd1110eb27386d8013ff104d7bc5702
Sridevi <sridevi073@gmail.com>
2016-12-05 18:40:28 +0530

    ads report

--- ads_report.php
+++ ads_report.php
@@ -1,6 +1,9 @@
 <?php
 include_once("./config/config.php");
 
+include("./functions/cms.php");
+
+$cms=new cms();
 
 $sql="select * from roo_ads";
 $qry=$db->query($sql);
@@ -28,8 +31,8 @@ while($row=$db->fetch_array($qry))
               } 
 			  
 			$query = "SELECT COUNT(t.adid) AS cnt FROM roo_transaction AS t WHERE (adid='$id')"; 
-            $qryCount = $this->db->query($query);
-		    $rowCount = $this->db->fetch_array($qryCount);
+            $qryCount = $db->query($query);
+		    $rowCount = $db->fetch_array($qryCount);
 			
 			
 			$totalviews=$rowCount['cnt'];
@@ -37,69 +40,69 @@ while($row=$db->fetch_array($qry))
 			$date=date('Y-m-d');
 			
 			$query2 = "SELECT COUNT(t.adid) AS cnt FROM roo_transaction AS t WHERE (date_added='$date' and adid='$id')"; 
-            $qryCount2 = $this->db->query($query2);
-		    $rowCount2 = $this->db->fetch_array($qryCount2);
+            $qryCount2 = $db->query($query2);
+		    $rowCount2 = $db->fetch_array($qryCount2);
 			
 			$todayviews=$rowCount2['cnt'];
 			
 			
-			$query3 = "SELECT email,firstname FROM roo_users WHERE (id='$userid')"; 
-            $qryCount3 = $this->db->query($query3);
-		    $rowCount3 = $this->db->fetch_array($qryCount3);
+			$query3 = "SELECT email,username FROM roo_admin_users WHERE (id='$userid')"; 
+            $qryCount3 = $db->query($query3);
+		    $rowCount3 = $db->fetch_array($qryCount3);
 			
-			
+			$adminmail=$cms->getsetting('1','email');
 			
 			$to = array($rowCount3['email']);
-			$from = 'info@roophka.com';
+			$from = $adminmail;
 			$subject = "Ad report from roophka.com";
 			$message = '<div style="width:600px;">
-			Dear '.$rowCount3['firstname'].'<br>
+			Dear '.$rowCount3['username'].'<br>
 			<p>Welcome to ROOPHKA.COM</p>
-			<p>Your ads details are following</p>
-			<br>
+			<p>Your ads details are following :</p>
+			
 			<table cellpadding="0" cellspacing="0" border="0">
 				<tr>
-					<td align="right"><h4>Adname : </h4> </td>
+					<td align="right"><strong>Adname : </strong> </td>
 					<td>'.$adname.'</td>
 				</tr>
 				
 				<tr>
-					<td align="right"><h4>Ad Title : </h4> </td>
+					<td align="right"><strong>Ad Title : </strong> </td>
 					<td>'.$addtitle.'</td>
 				</tr>
 				
 				<tr>
-					<td align="right"><h4>Watch count : </h4> </td>
+					<td align="right"><strong>Watch count : </strong> </td>
 					<td>'.$adwatch.'</td>
 				</tr>
 				
 				<tr>
-					<td align="right"><h4>Clicks Remain : </h4> </td>
+					<td align="right"><strong>Clicks Remain : </strong> </td>
 					<td>'.$adclicks.'</td>
 				</tr>
 				
 				<tr>
-					<td align="right"><h4>Ad Duration : </h4> </td>
+					<td align="right"><strong>Ad Duration : </strong> </td>
 					<td>'.$adduration.'</td>
 				</tr>
 				
 				<tr>
-					<td align="right"><h4>Amount : </h4> </td>
+					<td align="right"><strong>Amount : </strong> </td>
 					<td>'.$adamount.'</td>
 				</tr>
 				
 				<tr>
-					<td align="right"><h4>Status : </h4> </td>
+					<td align="right"><strong>Status : </strong> </td>
 					<td>'.$status.'</td>
 				</tr>
 				
 				<tr>
-					<td align="right"><h4>Total view count : </h4> </td>
+					<td align="right"><strong>Total view count : </strong> </td>
 					<td>'.$totalviews.'</td>
 				</tr>
 				
 				<tr>
-					<td align="right"><h4>Today view count : </h4> </td>
+					<td align="right"><strong>Today view count : </strong> </td>
 					<td>'.$todayviews.'</td>
 				</tr>
 				
@@ -108,6 +111,9 @@ while($row=$db->fetch_array($qry))
 			Thanks & regards,<br>
 			<a href="roophka.com">roophka.com</a>
 			</div>';
+			
+			//echo $rowCount3['email']."<br>".$from."<br>".$subject."<br>".$message;
+			
 			
 			$mailler->sendmail($to, $from, $subject, $message);
 			
