260b9520b8726b450b700fe7440113b5406fd66b
Sridevi <sridevi073@gmail.com>
2017-03-23 13:56:34 +0530

    aa

--- app.php
+++ app.php
@@ -7,9 +7,6 @@ include("./functions/cms.php");
 
 $output = array('response_code' => -1, 'error' => 'Invalid Request', 'session_id'=>'','msg' => 'No action performed for your request, Please send valid information.');
 
-include("./functions/location.php");
-include("./functions/cms.php");
-
 $cms=new cms();
 
 if((isset($_REQUEST['action']))&&($_REQUEST['appkey']=='Roo2017App')) {
@@ -451,8 +448,7 @@ if((isset($_REQUEST['action']))&&($_REQUEST['appkey']=='Roo2017App')) {
 		}
 		echo "[".json_encode($output)."]";exit;
 	}
-	echo "[".json_encode($output)."]"; exit;
-}
+	
 	
 	if($_REQUEST['action'] == 'advertiseus')
 	{
@@ -630,44 +626,7 @@ if((isset($_REQUEST['action']))&&($_REQUEST['appkey']=='Roo2017App')) {
 		echo "[".json_encode($output)."]";exit;
 	}
 	
-	if($_REQUEST['action'] == 'login')
-	{
-		$output = array('error' => '', 'session_id'=>'','msg' => '');
-		$user = $db->escape_string($_REQUEST['username']);
-	$pass = $db->escape_string($_REQUEST['password']);
 	
-	if($user == '' || $pass == '') {
-		//redirect(HTTP_PATH . 'login.php?error=1');
-		$output['error']="empty";
-	}
-	$pass = enc_password($pass);
-	
-	//echo "SELECT id,email,firstname,account_balance,lastlogin,status,phone,demo,spl_recharge FROM `roo_users` WHERE email='".$user."' AND pass='".$pass."'"; exit;
-	
-	$userQry = $db->query("SELECT id,email,firstname,account_balance,lastlogin,status,phone,demo,spl_recharge FROM `roo_users` WHERE email='".$user."' AND pass='".$pass."'");
-	$num = $db->num_rows($userQry);
-	if($num == 1) {
-		$userRow = $db->fetch_array($userQry);
-		
-		if($userRow['status'] > 0) {
-			$output['error']="inactive";
-		}else{
-		
-		$db->query("UPDATE `roo_users` SET lastlogin='".DATETIME24H."' WHERE id='".$userRow['id']."'");
-		
-		$output['session_id'] = $userRow['id'];
-				
-		//redirect(HTTP_PATH . 'dashboard.php');
-	
-	   $output['msg']="success";
-	}
-	}
-		
-	 else {
-		$output['error']="not exist";
-	}
-	echo "[".json_encode($output)."]";exit;
-	}
 }
 
 sendJson(-1);
