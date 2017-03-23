86865c1d64ad0653c278be9aa21b227f66ea2963
Sridevi <sridevi073@gmail.com>
2017-03-23 13:40:09 +0530

    app

--- app.php
+++ app.php
@@ -4,25 +4,7 @@ include("./functions/register.php");
 include("./functions/location.php");
 include("./functions/cms.php");
 
-<<<<<<< HEAD
-$reg = new register();
-$cms=new cms();
 
-if((isset($_REQUEST['action']))&&($_REQUEST['appkey']=='Roo2017App'))
-	{
-
-	if($_REQUEST['action'] == 'login')
-	{
-		
-	$output = array('error' => '', 'session_id'=>'','msg' => '');
-		
-	$user = $db->escape_string($_REQUEST['username']);
-	$pass = $db->escape_string($_REQUEST['password']);
-	
-	if($user == '' || $pass == '') {
-		//redirect(HTTP_PATH . 'login.php?error=1');
-		$output['error']="empty";
-=======
 $output = array('response_code' => -1, 'error' => 'Invalid Request', 'session_id'=>'','msg' => 'No action performed for your request, Please send valid information.');
 
 include("./functions/location.php");
@@ -69,7 +51,6 @@ if((isset($_REQUEST['action']))&&($_REQUEST['appkey']=='Roo2017App')) {
 			$data['image'] = $ads->getAdlist("image");
 		}
 		sendJson(1,$data);
->>>>>>> 1cf9897db4ae51833e5afc63d5564de2cdf6c729
 	}
 
 
@@ -205,8 +186,6 @@ if((isset($_REQUEST['action']))&&($_REQUEST['appkey']=='Roo2017App')) {
 		$database['account_balance'] = $user->getAccountBalance($_REQUEST['session_id']);
 		sendJson(1,$database);
 	}
-<<<<<<< HEAD
-=======
 
 	/****
 	 * get list of transaction details
@@ -470,9 +449,6 @@ if((isset($_REQUEST['action']))&&($_REQUEST['appkey']=='Roo2017App')) {
 			$output['error']=true;
 			$output['msg']="Error, please try again !!!";
 		}
->>>>>>> 1cf9897db4ae51833e5afc63d5564de2cdf6c729
-		
-		
 		echo "[".json_encode($output)."]";exit;
 	}
 	echo "[".json_encode($output)."]"; exit;
@@ -555,51 +531,6 @@ if((isset($_REQUEST['action']))&&($_REQUEST['appkey']=='Roo2017App')) {
 	
 	if($_REQUEST['action'] == 'register')
 	{
-<<<<<<< HEAD
-		
-		$reg->name = $db->escape_string($_REQUEST['name']);
-	$reg->email = $db->escape_string($_REQUEST['email']);
-	$reg->password = $db->escape_string($_REQUEST['password']);
-	$reg->mobile = $db->escape_string($_REQUEST['mobile']);
-	$reg->dob = $db->escape_string($_REQUEST['dob']);
-	$reg->address = $db->escape_string($_REQUEST['address']);
-	$reg->state = $db->escape_string($_REQUEST['state']);
-	$reg->city = $db->escape_string($_REQUEST['city']);
-	$reg->pincode = $db->escape_string($_REQUEST['pincode']);
-	
-	$result = $reg->save();
-  
-	if($result['error']) {
-		if($result['msg'] == 'empty')
-			$result['error']="All input fields are mandatory";
-		elseif($result['msg'] == 'insert')
-			$result['msg']="Not inserted";
-		elseif($result['msg'] == 'duplicate')
-			$result['msg']="Email already exists";
-		else
-			$result['error']="Not inserted";
-	}else {
-		$result['error']=false;
-			$result['msg']="Inserted Successfully";
-		$to = array($reg->email);
-		$from = 'info@roophka.com';
-		$subject = "Roophka : Registration complete.";
-    $encarray = array('userid'=>$result['userid'], 'action'=>'verify', 'type'=>'email');
-    $enc = three_layer_encrypt('',$encarray);
-    $message = '<div style="width:600px;">
-    Dear '.$reg->name.'<br>
-    <p>Welcome to ROOPHKA.COM</p>
-    <p>Please verify your mail address by clicking below link.</p>
-    <a href="'.HTTP_PATH.'userlogin.php?enc='.$enc.'">Click here to verify</a><br /><br />
-    Thanks & regards,<br />
-    <a href="'.HTTP_PATH.'">roophka.com</a>
-    </div>';
-		
-		$mailler->sendmail($to, $from, $subject, $message);
-		$result['verify'] == 'Please check and verify your email';
-	}
-	echo "[".json_encode($result)."]";exit;
-=======
 		include("./functions/register.php");
 		$reg = new register();
 		
@@ -645,8 +576,7 @@ if((isset($_REQUEST['action']))&&($_REQUEST['appkey']=='Roo2017App')) {
 			//$result['verify'] = 'Please check and verify your email';
 		}
 		echo "[".json_encode($result)."]";exit;
->>>>>>> 1cf9897db4ae51833e5afc63d5564de2cdf6c729
-	}
+     }
 	
 	if($_REQUEST['action']=='state')
 	{
@@ -700,9 +630,45 @@ if((isset($_REQUEST['action']))&&($_REQUEST['appkey']=='Roo2017App')) {
 		echo "[".json_encode($output)."]";exit;
 	}
 	
+	if($_REQUEST['action'] == 'login')
+	{
+		$output = array('error' => '', 'session_id'=>'','msg' => '');
+		$user = $db->escape_string($_REQUEST['username']);
+	$pass = $db->escape_string($_REQUEST['password']);
+	
+	if($user == '' || $pass == '') {
+		//redirect(HTTP_PATH . 'login.php?error=1');
+		$output['error']="empty";
+	}
+	$pass = enc_password($pass);
+	
+	//echo "SELECT id,email,firstname,account_balance,lastlogin,status,phone,demo,spl_recharge FROM `roo_users` WHERE email='".$user."' AND pass='".$pass."'"; exit;
+	
+	$userQry = $db->query("SELECT id,email,firstname,account_balance,lastlogin,status,phone,demo,spl_recharge FROM `roo_users` WHERE email='".$user."' AND pass='".$pass."'");
+	$num = $db->num_rows($userQry);
+	if($num == 1) {
+		$userRow = $db->fetch_array($userQry);
+		
+		if($userRow['status'] > 0) {
+			$output['error']="inactive";
+		}else{
+		
+		$db->query("UPDATE `roo_users` SET lastlogin='".DATETIME24H."' WHERE id='".$userRow['id']."'");
+		
+		$output['session_id'] = $userRow['id'];
+				
+		//redirect(HTTP_PATH . 'dashboard.php');
+	
+	   $output['msg']="success";
+	}
+	}
+		
+	 else {
+		$output['error']="not exist";
+	}
+	echo "[".json_encode($output)."]";exit;
+	}
 }
-<<<<<<< HEAD
-=======
+
 sendJson(-1);
->>>>>>> 1cf9897db4ae51833e5afc63d5564de2cdf6c729
 ?>
